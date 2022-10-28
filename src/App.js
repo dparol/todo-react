fc6429ee588311f37878e8b4178e887c26c56a46
dparol <dikshithparol123@gmail.com>
2022-10-28 10:07:28 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [ToDos,setToDos]=useState([])
+  let [ToDo,setToDo]=useState('')
+ let remove=(id) =>{
+  console.log(id);
+  setToDos(ToDos.filter((item,index)=>id!==item.id))
+
+ }
+
+ console.log(ToDos);
+
+
+ 
+return (
+  <div className="app">
+  <div className="mainHeading">
+    <h1>ToDo List</h1>
+  </div>
+  <div className="subHeading">
+    <br />
+    <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+  </div>
+  <div className="input">
+    <input value={ToDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+    <i onClick={()=>setToDos([{value:ToDo,id:Date.now(),status:false},...ToDos])} className="fas fa-plus"></i>
+  </div>
+  <div style={{display:'flex'}}>
+    
+  
+  <div className="todos">
+    {ToDos.map((value,index)=>{
+      return(
+        <div key={index} className="todo">
+        <div className="left">
+          <input onClick={(e)=>{setToDos(ToDos.filter((item)=> { 
+            if(item.id===value.id){
+            console.log(e.target.checked);
+             item.status=e.target.checked;
+          }
+          console.log(item.id);
+          return item
+          })
+          )}
+          } type="checkbox" name="" id="" />
+          <p>{value.value}</p>
+        </div>
+        <div className="right">
+          <i onClick={()=>remove(value.id)} className="fas fa-times"></i>
+        </div>
+      </div>
+      )
+
+    })}
+   
+  </div>
+  <div>
+    {ToDos.map((value)=>{
+      if(value.status)
+      {
+        return (
+        <p style={{background:"white"}}>{value.value}</p>
+      )
+    }
+      
+    })}
+  </div>
+  </div>
+</div>
+);
 }
 
 export default App;
---
+++ src/P.js
@@ -0,0 +1,10 @@
+import React from "react";
+const P =(prop)=>{
+
+    return(<p>
+       
+        {prop.text[prop.count]}
+        
+    </p>)
+}
+export default P
